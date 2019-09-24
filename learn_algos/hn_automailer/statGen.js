d74c9e2041ea62f9c17def6fe92548ee73fbe815
Kaustav Haldar <hi@kaustav.me>
2019-09-24 12:10:25 -0400

    Added a stat generator

---
+++ learn_algos/hn_automailer/statGen.js
@@ -0,0 +1,23 @@
+const { execSync } = require('child_process');
+
+let postUrlIds = [
+	'18807017',
+	'19055166',
+	'19281834',
+	'19543940',
+	'19797594',
+	'20083795',
+	'20325925',
+	'20584311',
+	'20867123'
+]
+let genStatsOverYear = () => {
+	for (let i=0; i<postUrlIds.length; i++) {
+        let hnurlid = postUrlIds[i];
+        console.log("current month: " + i);
+		console.log('current hn url id: ', hnurlid);
+		let child = execSync('node main.js '+hnurlid, { encoding : 'utf8' });
+		console.log(child);
+	}
+}
+genStatsOverYear();
