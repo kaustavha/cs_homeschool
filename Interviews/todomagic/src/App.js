9b037487bf23f69edbc093cb93c27e978b8092b2
Kaustav Haldar <hi@kaustav.me>
2019-10-17 18:14:24 -0400

    clean up magic todolist code a bit

--- Interviews/todomagic/src/App.js
+++ Interviews/todomagic/src/App.js
@@ -11,9 +11,8 @@ import './App.css';
  */
 
 class listItem {
-  constructor(name, children){
+  constructor(name){
     this.name = name;
-    this.children = children;
   }
 }
 
@@ -24,22 +23,50 @@ function TodoInput(props) {
   const handleSubmit = (evt) => {
     evt.preventDefault();
     setNames(names => {
-      console.log(names)
       names.push(new listItem(name));
+      setName("");
       return names;
     });
   }
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <label>Add a todo item {props.under ? `Under ${props.under}` : ''}</label>
+        <label>Add a todo item {props.under ? `under ${props.under}` : ''}</label>
         <input type="text" value={name} onChange={e=>setName(e.target.value)} />
         <input type='submit' value="Submit" />
       </form>
       <div>
-      {names.map(li => <TodoItem key={li.name} name={li.name} spacing={spacing} names={names} setNames={setNames}/>)}
+      {names.map(li => <TodoItem under={props.under} key={li.name} name={li.name} spacing={spacing} setNames={setNames}/>)}
       </div>
     </div>
+  )
+}
+
+function TodoItem(props) {
+  const name = props.name,
+        spacing = props.spacing+50,
+        under = props.under ? props.under + '/' + name : name,
+        divStyle = {
+          marginLeft: spacing
+        };
+  return (<div style={divStyle}>
+    <DoneButton name={name} setNames={props.setNames} />
+    <p>{name}</p>
+    <TodoInput under={under} spacing={spacing}/>
+  </div>)
+}
+
+function DoneButton(props) {
+  let setNames = props.setNames;
+  let curName = props.name;
+  const handleDone = (ev) => {
+    ev.preventDefault();
+    setNames(items => items.filter(item => item.name !== curName));
+  }
+  return (
+    <form onSubmit={handleDone}>
+    <input type='submit' value="Done" />
+  </form>
   )
 }
 
@@ -47,8 +74,6 @@ function UsernameInput(props) {
   const [username, setUsername] = useState('');
   const [seenUsernames, setSeenUsernames] = useState([]);
   const [loggedIn, setLoggedIn] = useState(false);
-  // let loggedIn = false;
-  console.log('login', loggedIn, username, loggedIn)
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
@@ -81,46 +106,9 @@ function UsernameInput(props) {
     <label>Hi whats your name? </label>
     <input type="text" value={username} onChange={e=>setUsername(e.target.value)} />
     <input type='submit' value="Submit" />
-  </form>
+    </form>
   )
   
-}
-
-// todo - input and del
-function TodoItem(props) {
-  const name = props.name,
-    spacing = props.spacing;
-  const divStyle = {
-    marginLeft: spacing
-  }
-  return (<div style={divStyle}>
-
-    <DoneButton name={name} setNames={props.setNames} names={props.names} />
-    <p>{name}</p>
-    <TodoInput under={name} spacing={spacing+50}/>
-  </div>)
-}
-
-function DoneButton(props) {
-  let setNames = props.setNames;
-  let curName = props.name;
-  let names = props.names;
-  const handleDone = (ev) => {
-    ev.preventDefault();
-    let savedI = null;
-    names.forEach((name, i) => {
-      if (name === curName) {
-        savedI = i;
-      }
-    });
-    names.splice(savedI, 1);
-    setNames([...names]);
-  }
-  return (
-    <form onSubmit={handleDone}>
-    <input type='submit' value="Done" />
-  </form>
-  )
 }
 
 function App() {
@@ -129,7 +117,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <UsernameInput />
-        {/* <TodoInput spacing={0}/> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
